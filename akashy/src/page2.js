81c9d4f3401b326e742445c9589928435820bde0
Shashank <shashank08090@gmail.com>
2023-04-02 13:10:44 +0530

    change 1

--- akashy/src/page2.js
+++ akashy/src/page2.js
@@ -1,11 +1,11 @@
-import './page2.css'
+import "./page2.css";
 
-const Page2  =  () =>{
-return (<div id="page2">
-<h1> hello page 2 web</h1>
+const Page2 = () => {
+  return (
+    <div id="page2">
+      <h1> hello page 2 web change 1</h1>
+    </div>
+  );
+};
 
-</div>)
-}
-
-
-export default Page2
+export default Page2;
