24d15de6ac499f00bc48ece253fddbadf872276a
Shashank <shashank08090@gmail.com>
2023-04-06 10:59:27 +0530

    try 1

--- akashy/src/Apps.js
+++ akashy/src/Apps.js
@@ -1,9 +1,20 @@
-import Student from "./student";
 function App() {
   return (
     <div className="App">
-      <h1>props!</h1>
-      <Student />
+      <h1>Handle From in React</h1>
+      <form>
+        <input type="text" placeholder="enter name" /> <br />
+        <br />
+        <select>
+          <option>select option</option>
+          <option>Marvel</option>
+          <option>Dc</option>
+        </select>
+        <br />
+        <br />
+        <input type="checkbox" />
+        <span>Accept Terms and conditions</span>
+      </form>
     </div>
   );
 }
