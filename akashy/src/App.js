6ed3011acba72df9966897339c2388b39d615bd4
Shashank <shashank08090@gmail.com>
2023-04-08 10:05:53 +0530

    try4

---
+++ akashy/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+function App() {
+  const [data, setData] = useState(null);
+  function getData(val) {
+    console.warn(val.target.value);
+    setData(val.target.value);
+  }
+  return (
+    <div className="App">
+      <h1>{data}</h1>
+      <input type="text" onChange={getData} />
+    </div>
+  );
+}
+export default App;
--- akashy/src/Apps.js
+++ akashy/src/Apps.js
@@ -1,20 +1,11 @@
+import React, { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    console.warn("useEffect");
+  });
   return (
     <div className="App">
-      <h1>Handle From in React</h1>
-      <form>
-        <input type="text" placeholder="enter name" /> <br />
-        <br />
-        <select>
-          <option>select option</option>
-          <option>Marvel</option>
-          <option>Dc</option>
-        </select>
-        <br />
-        <br />
-        <input type="checkbox" />
-        <span>Accept Terms and conditions</span>
-      </form>
+      <h1> useEffect in React</h1>
     </div>
   );
 }
