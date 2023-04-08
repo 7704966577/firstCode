b64279bb9261372992b1f0e0c9fc1b52d3985fbc
Shashank <shashank08090@gmail.com>
2023-04-08 10:09:59 +0530

    test

--- akashy/src/index.js
+++ akashy/src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-import App from "./Apps";
-ReactDom.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import App from "./App";
+// import Demo from "./Demo";
+
+ReactDom.render(<App />, document.getElementById("root"));
